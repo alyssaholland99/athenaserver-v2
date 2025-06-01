25aa628cf47ce9f29f38783327efaf77ae1745e4
Alyssa Holland <2011hollanda@gmail.com>
2025-06-01 11:35:30 +0100

    testing permissions

--- helpers/checkpermissions.py
+++ helpers/checkpermissions.py
@@ -14,6 +14,8 @@ def getPermission(user):
         return -1
         
 def checkPermission(user, requiredPermission):
+    print(getPermission(user))
+    print( requiredPermission)
     return getPermission(user) >= requiredPermission
 
 def setPermission(user, newPermission):
