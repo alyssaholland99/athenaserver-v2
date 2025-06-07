0aa63692b5408351628885e76507d860d0522cbb
Alyssa Holland <2011hollanda@gmail.com>
2025-06-07 02:45:04 +0100

    Made return -1 when no user is defined

--- helpers/checkpermissions.py
+++ helpers/checkpermissions.py
@@ -4,9 +4,13 @@ with open('configs/service_info.json') as f:
     json_data = json.load(f)
 
 def getUserDetails(user_id):
+    if not str(user_id in json_data):
+        return -1
     return int(json_data[str(user_id)])
 
 def getUserPermissions(user_id):
+    if getUserDetails(user_id) == -1:
+        return -1
     return getUserDetails(user_id)["level"]
 
 def checkPermission(user_id, requiredPermission):
