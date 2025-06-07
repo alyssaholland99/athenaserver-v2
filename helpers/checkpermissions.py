8ac21030db78b8c719d4561d3d5b49b1c5927503
Alyssa Holland <2011hollanda@gmail.com>
2025-06-07 02:57:01 +0100

    Maybe fixed user permissions

--- helpers/checkpermissions.py
+++ helpers/checkpermissions.py
@@ -10,16 +10,14 @@ def getUserDetails(user_id):
     except:
         return -1
     
-
-def getUserPermissions(user_id):
-    if getUserDetails(user_id) == -1:
+def getUserLevel(user_id):
+    try:
+        return int(json_data[str(user_id)]["level"])
+    except:
         return -1
-    return getUserDetails(user_id)["level"]
 
 def checkPermission(user_id, requiredPermission):
-    print(getUserPermissions(user_id))
-    print(requiredPermission)
-    return getUserPermissions(user_id) >= requiredPermission
+    return getUserLevel(user_id) >= requiredPermission
 
 def setPermission(user_id, requiredPermission):
     print("test")
